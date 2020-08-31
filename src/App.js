1d9af181b7ed50a2293a33b0f5efaf7e1accc0ab
Drew Vlasnik <314dsv@gmail.com>
2020-08-31 17:27:10 -0400

    Adjust layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import SortingVisualizer from "./components/SortingVisualizer";
+import { Jumbotron } from "reactstrap";
 
 function App() {
   return (
     <Fragment>
+      <div className="container mt-2">
+        <h1>Sorting Visualizer</h1>
+      </div>
       <SortingVisualizer />
     </Fragment>
   );
--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -10,6 +10,7 @@ const SECONDARY_COLOR = "#ff5c5c";
 
 const SortingVisualizer = () => {
   const [array, setArray] = useState([]);
+  const [animate, setAnimate] = useState(true);
 
   const genNewArray = () => {
     const array = [];
@@ -25,26 +26,7 @@ const SortingVisualizer = () => {
     genNewArray();
   }, []);
 
-  // Sorting algorithm helper functions
-  const mergeSort = async () => {
-    const animations = MergeSort(array);
-    console.log(array);
-    for (let i = 0; i < animations.length; i++) {
-      const bars = document.getElementsByClassName("array-bar");
-      const [barOneIdx, barTwoIdx] = animations[i].compared;
-      const barOneStyle = bars[barOneIdx].style;
-      const barTwoStyle = bars[barTwoIdx].style;
-      barOneStyle.backgroundColor = SECONDARY_COLOR;
-      barTwoStyle.backgroundColor = SECONDARY_COLOR;
-      // setTimeout() failed to work here - color would switch back instantly
-      await sleep(ANIMATION_SPEED);
-      const [idx, newHeight] = animations[i].replaced;
-      barOneStyle.backgroundColor = PRIMARY_COLOR;
-      barTwoStyle.backgroundColor = PRIMARY_COLOR;
-      bars[idx].style.height = `${newHeight}px`;
-    }
-  };
-
+  // Sorting algorithm animation functions
   const bubbleSort = async () => {
     const animations = BubbleSort(array);
     console.log(array);
@@ -67,6 +49,27 @@ const SortingVisualizer = () => {
       barTwoStyle.backgroundColor = PRIMARY_COLOR;
     }
   };
+
+  const mergeSort = async () => {
+    const animations = MergeSort(array);
+    console.log(array);
+    for (let i = 0; i < animations.length; i++) {
+      const bars = document.getElementsByClassName("array-bar");
+      const [barOneIdx, barTwoIdx] = animations[i].compared;
+      const barOneStyle = bars[barOneIdx].style;
+      const barTwoStyle = bars[barTwoIdx].style;
+      barOneStyle.backgroundColor = SECONDARY_COLOR;
+      barTwoStyle.backgroundColor = SECONDARY_COLOR;
+      // setTimeout() failed to work here - color would switch back instantly
+      await sleep(ANIMATION_SPEED);
+      const [idx, newHeight] = animations[i].replaced;
+      barOneStyle.backgroundColor = PRIMARY_COLOR;
+      barTwoStyle.backgroundColor = PRIMARY_COLOR;
+      bars[idx].style.height = `${newHeight}px`;
+    }
+  };
+
+  const quickSort = async () => {};
 
   const testSort = () => {
     const arr = [];
@@ -99,28 +102,30 @@ const SortingVisualizer = () => {
 
   return (
     <Fragment>
-      <div className="m-2">
-        {array.map((value, idx) => (
-          <div
-            className="array-bar"
-            key={idx}
-            style={{ height: `${value}px` }}
-          ></div>
-        ))}
-      </div>
-      <div className="m-2">
-        <Button color="danger" onClick={() => genNewArray()}>
-          New Array
-        </Button>
-        <Button color="success" onClick={() => mergeSort()}>
-          Merge Sort
-        </Button>
-        <Button color="success" onClick={() => bubbleSort()}>
-          Bubble Sort
-        </Button>
-        <Button color="warning" onClick={() => testSort()}>
-          Test Sorting
-        </Button>
+      <div className="container mt-5">
+        <div className="m-2">
+          {array.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{ height: `${value}px` }}
+            ></div>
+          ))}
+        </div>
+        <div className="m-2">
+          <Button color="danger" onClick={() => genNewArray()}>
+            New Array
+          </Button>{" "}
+          <Button color="success" onClick={() => bubbleSort()}>
+            Bubble Sort
+          </Button>{" "}
+          <Button color="success" onClick={() => mergeSort()}>
+            Merge Sort
+          </Button>{" "}
+          <Button color="warning" onClick={() => testSort()}>
+            Test Sorting
+          </Button>{" "}
+        </div>
       </div>
     </Fragment>
   );
