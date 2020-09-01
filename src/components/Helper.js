6251b1f4f2e011f5ece8c4d8660494d17fae9b70
Drew Vlasnik <314dsv@gmail.com>
2020-09-01 12:45:43 -0400

    Add comments

--- src/components/Helper.js
+++ src/components/Helper.js
@@ -1,15 +1,27 @@
 import BubbleSort from "./BubbleSort";
 import MergeSort from "./MergeSort";
 import QuickSort from "./QuickSort";
+import InsertionSort from "./InsertionSort";
 
 export const testSort = () => {
+  // Create arr of random length filled with random numbers
   const arr = [];
   const len = randInt(10, 1000);
   for (let i = 0; i < len; i++) {
     arr.push(randInt(-1000, 1000));
   }
+  console.log(arr);
+
+  // Sorted copy of arr
   const jsSortedArr = arr.slice().sort((a, b) => a - b);
-  QuickSort(arr);
+
+  // Sort to test
+  // BubbleSort(arr);
+  // MergeSort(arr);
+  // QuickSort(arr);
+  // InsertionSort(arr);
+
+  console.log(arr);
   console.log(equalArr(jsSortedArr, arr));
 };
 
